1a1a70ab6da12564d1fcbcd35ea443c280e5db4c
Shah Rukh <shah.xhtml@yahoo.com>
2022-11-27 23:13:37 +0530

    instruction for user inputs

--- lib/autoforward/variables.py
+++ lib/autoforward/variables.py
@@ -3,5 +3,5 @@ from decouple import config
 APP_ID = config("FORWARDER_APP_ID", default=None, cast=int)
 API_HASH = config("FORWARDER_API_HASH", default=None)
 SESSION = config("FORWARDER_SESSION")
-FORWARDER_FROM_CHANNEL = config("FORWARDER_FROM_CHANNEL")
-FORWARDER_TO_CHANNEL = config("FORWARDER_TO_CHANNEL")
+FORWARDER_FROM_CHANNEL = config("FORWARDER_FROM_CHANNEL") # multiple inputs separated by commas, ID's or username without '@'
+FORWARDER_TO_CHANNEL = config("FORWARDER_TO_CHANNEL") # multiple inputs separated by commas, ID's or username without '@'
