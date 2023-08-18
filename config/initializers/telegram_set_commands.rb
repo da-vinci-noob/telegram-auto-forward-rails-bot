d70e0891685733cba91d7b56e16822e46c706c70
Shah Rukh <shah.xhtml@yahoo.com>
2023-08-19 01:21:17 +0530

    dummy set commands file

---
+++ config/initializers/telegram_set_commands.rb
@@ -0,0 +1,18 @@
+commands = {
+  commands: [
+    { command: 'start', description: 'Starts the bot' },
+    { command: 'help', description: 'View available commands with description' },
+    { command: 'amazon', description: 'add amazon tracking id' },
+    { command: 'flipkart', description: 'add flipkart tracking id' },
+    { command: 'bitly_setup', description: 'show Bit.ly setup procedure' },
+    { command: 'bitly', description: 'add bitly access token' },
+    { command: 'previews', description: 'For disabling Link Previews' },
+    { command: 'forward', description: 'Add @channel to forward messages to' },
+    { command: 'delete', description: 'Add word to delete from return message' },
+    { command: 'show_deleted', description: 'Show Your words to delete' },
+    { command: 'remove_deleted', description: 'Remove a keyword from delete list' },
+    { command: 'user_profile', description: 'Show Your Complete Configuration' }
+  ], language_code: 'en'
+}
+
+# Telegram.bot.set_my_commands(commands)
